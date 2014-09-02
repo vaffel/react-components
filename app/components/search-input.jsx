eb6056a7b242393af17d59d3fa12dcf3689fc9ff
Espen Hovlandsdal <rexxars@gmail.com>
2014-09-03 00:18:12 +0200

    Fixed page title

--- app/components/search-input.jsx
+++ app/components/search-input.jsx
@@ -39,7 +39,7 @@ module.exports = React.createClass({
     },
 
     getPageTitle: function(query) {
-        return config['page-title'] + (query ? (' - ' + query) : '');
+        return (query ? (query + ' - ') : '') + config['page-title'];
     },
 
     moveCaretToEnd: function() {
