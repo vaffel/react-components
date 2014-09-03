20be12b16b1eda8a5355edbedf56c63e66519ef2
Espen Hovlandsdal <rexxars@gmail.com>
2014-09-03 20:01:04 +0200

    Show a mobile-optimized meta section in search results

--- app/components/results-item.jsx
+++ app/components/results-item.jsx
@@ -14,6 +14,17 @@ module.exports = React.createClass({
                 <td>
                     <ComponentLink component={this.props.component} />
                     <p className="description">{this.props.component.description}</p>
+
+                    <dl className="component-meta">
+                        <dt><i className="fa fa-star-o" title="Stars" /></dt>
+                        <dd>{this.props.component.stars || 0}</dd>
+
+                        <dt><i className="fa fa-calendar" title="Modified" /></dt>
+                        <dd>{this.props.component.modified.fromNow()}</dd>
+
+                        <dt><i className="fa fa-user" title="Author" /></dt>
+                        <dd>{this.props.component.author}</dd>
+                    </dl>
                 </td>
                 <td>{this.props.component.author}</td>
                 <td>{this.props.component.stars || 0}</td>
