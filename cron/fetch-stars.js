83c542f3f6dc47dd833f40ab4fdcbf643e7f6e94
Espen Hovlandsdal <rexxars@gmail.com>
2015-05-18 19:25:56 +0200

    Add debug information to the github star fetcher

--- cron/fetch-stars.js
+++ cron/fetch-stars.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var debug = require('debug')('cron');
 var db = require('app/database');
 var async = require('async');
 var GithubApi = require('app/api/github-api');
@@ -23,9 +24,11 @@ db.getModules(function(err, modules) {
 function getStarCountAndUpdate(module, callback) {
     GithubApi.getStarCountForModule(module, function(err, starCount) {
         if (err) {
+            debug('Failed to fetch star count for module "%s" - %s', module.name, err.message);
             return callback();
         }
 
+        debug('Star count for module "%s": %d', module.name, starCount);
         db.setModuleStars(module._id, starCount, callback);
     });
 }
